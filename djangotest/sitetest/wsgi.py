d5a1db6e72119495e43050b6c9edf7bb8660d5aa
Bubble115 <gradymike115@gmail.com>
2025-05-22 12:16:15 +0700

    Update wsgi.py

--- djangotest/sitetest/wsgi.py
+++ djangotest/sitetest/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Add the project root directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sitetest.settings")
 
 application = get_wsgi_application()
